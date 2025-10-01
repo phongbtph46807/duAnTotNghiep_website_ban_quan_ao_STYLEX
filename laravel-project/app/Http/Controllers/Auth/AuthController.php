c843c83b7f39d011450da8e74ce0af840292b986
phongbtph46807 <phongbtph46807.fpt.edu>
2025-10-02 03:41:52 +0700

    update-verfication-mail-and-login-form

--- laravel-project/app/Http/Controllers/Auth/AuthController.php
+++ laravel-project/app/Http/Controllers/Auth/AuthController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -27,8 +34,53 @@ class AuthController extends Controller
             $user->email = $request->email;
             $user->phone_number = $request->phone_number;
             $user->password = Hash::make($request->password);
+            $user->verification_token = Str::random(60);
+            $user->token_expires_at = Carbon::now()->addHour();
             $user->save();
-            return back()->with('success', 'Đăng Kí Thành Công !!');
+
+            $this->sendVerificationMail($user);
+            return back()->with('success', 'Đăng Kí Thành Công. Hãy check mail của bạn nhé !!');
+        } catch (\Exception $e) {
+            return back()->with('error', $e->getMessage());
+        }
+    }
+
+    protected function sendVerificationMail($user)
+    {
+        $verificationUrl = url('/verify/'.$user->verification_token);
+        Mail::send('mails.verification',['name' => $user->name, 'url' => $verificationUrl], function($message) use ($user){
+        $message->to($user->email);  
+        $message->subject('Xác thực email');  
+
+    });
+    }
+
+        public function loginView(){
+        try {
+            //code...
+            return view('auth.login');
+        } catch (\Exception $e) {
+            return abort(404, "Có gì đó không ổn!");
+        }
+    }
+
+        public function login(LoginRequest $request){
+        try {
+            //code...
+            $userLogin = $request->only('email', 'password');
+            if(Auth::attempt($userLogin)){
+                if(Auth::user()->is_verified == 0){
+                    Auth::logout();
+                    return back()->with('error', 'Hãy xác thực tài khoản của bạn!');
+                }
+                if(Auth::user()->is_admin == 1){
+                    return redirect()->route('admin.dashboard');
+                }else{
+                    return redirect()->route('user.dashboard');
+                }
+            }else{
+                 return back()->with('error', 'Username và Password không hợp lệ!');
+            }
         } catch (\Exception $e) {
             return back()->with('error', $e->getMessage());
         }
--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\VerificationController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/admin', function () {
-    return view('admin.home-admin');
-});
 
 Route::get('/register', [AuthController::class,'registerView'])->name('registerView');
 Route::post('/register', [AuthController::class,'register'])->name('register');
+
+Route::get('/verify/{token}', [VerificationController::class, 'verify'])->name('verify');
+
+Route::get('/login', [AuthController::class,'loginView'])->name('loginView');
+Route::post('/login', [AuthController::class,'login'])->name('login');
+
+Route::get('/dashboard', function(){
+    return 'User Dashboard';
+})->name('user.dashboard');
+
+Route::get('/admin/dashboard', function(){
+    return 'Admin Dashboard';
+})->name('admin.dashboard');
